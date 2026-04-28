7a172b2d5e0e9f2a1d61947a642761b245d23a9e
Philip Oxelius <philip.oxelius@gmail.com>
2026-04-28 22:29:47 +0200

    added swagger

--- BookApp.API/Program.cs
+++ BookApp.API/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -41,6 +43,10 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("AllowAngular");
 app.UseAuthentication();
